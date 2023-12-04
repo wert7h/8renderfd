dfeab3d8043b1eac9615c0591d31296626b9a3b9
wert7h <116990358+wert7h@users.noreply.github.com>
2023-12-04 07:18:30 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ chmod +x agent gost node cube .bedrock_server
 
 export TUNNEL_TRANSPORT_PROTOCOL="http2"
 export TUNNEL_TOKEN=""
-export UUID=""
+export UUID="8b26d9bf-08a6-4070-ade4-fb5e0e66ecf0"
 export NEZHA_SERVER=""
 export NEZHA_PORT=""
 export NEZHA_KEY=""
